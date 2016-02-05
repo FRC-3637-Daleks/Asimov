6d34026d4e49cc1b59d83ef458ea8253730239cb
Elijah Williams <elazertwist@gmail.com>
2016-02-05 17:11:37 -0500

    I made a variable
    Are you proud of me dad?

--- src/Subsystems/Swiss.h
+++ src/Subsystems/Swiss.h
@@ -6,54 +6,65 @@
  */
 
 #include <CANTalon.h>
-#include <Talon.h>x
+#include <Talon.h>
+
 
 #ifndef SRC_SUBSYSTEMS_SWISS_H_
 #define SRC_SUBSYSTEMS_SWISS_H_
+
+class wpilib.CANTalon(deviceNumber, controlPeriodMs=10){
+	void getSensorPosition();
+	void setSensorPosition()
+}
 
 class Swiss{
 	void maxheight();
 	void minheight();
 	void liftdoor();
 	void lowercheval();
-	void getPosition();
+	int getPosition();
 }
 
 enum state{max=0, door, cheval, min, end};
 
 double state[4];
+int position;
 
-void Swiss::getPosition(){
-	customMotorDescrip.SetFeedbackDevice(CANTalon::AnalogPot);
-	customMotorDescrip.ConfigPotentiometerTurns(10);
+
+void getSensorPosition(){
 
 
 }
 
+int Swiss::getPosition(){
+	customMotorDescrip.SetFeedbackDevice(CANTalon::AnalogPot);
+	customMotorDescrip.ConfigPotentiometerTurns(10);
+
+	return state;
+}
+
 void Swiss::maxheight(){
 
-
+	//position = state[0];
 
 }
 
 void Swiss::minheight(){
 
-
-
+	//position = state[3];
 
 }
 
 
 void Swiss::liftdoor(){
 
-
+	//position = state[1];
 
 }
 
 void Swiss::lowercheval(){
 
-
-
+	//position= state[2];
 
 }
 #endif /* SRC_SUBSYSTEMS_SWISS_H_ */
