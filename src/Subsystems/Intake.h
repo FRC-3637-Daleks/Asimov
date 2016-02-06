02e350f83424186b8df9feda889e33eb9f6bdb51
David Sevilla <sevilladavid83@gmail.com>
2016-02-06 09:39:38 -0500

    Updated comment documentation

--- src/Subsystems/Intake.h
+++ src/Subsystems/Intake.h
@@ -9,17 +9,19 @@
 #define SRC_SUBSYSTEMS_INTAKE_H_
 #include "WPILib.h"
 
-/** Commands namespace: has forward
- * declarations of command classes
+/**
+ * Commands namespace: has forward
+ * declarations of command classes.
  */
 namespace commands
 {
 	class IntakeBall;
 } // end namespace commands
 
-/** Subsystems namespace: contains
- * Intake class. Used to carry class
- * definition over to command files.
+/**
+ * Subsystems namespace for carying
+ * Intake class definition over to
+ * command files.
  */
 namespace subsystems
 {
@@ -29,48 +31,55 @@ class Intake : public Subsystem
 // Constructor and Destructor:
 public:
 	Intake();
-
 	virtual ~Intake();
 
 // Functions:
 public:
-	/** Returns true if the mechanical switch (detector_)is pressed on, and
-	 *  false otherwise. Used to determine whether or not to call Stop().
+	/**
+	 * Returns true if the mechanical switch (detector_) is
+	 * pressed on, and false otherwise. Used to determine
+	 * whether or not to call Stop().
 	 */
 	bool CheckSwitch() const;
 
-	/** Controls the front rollers to take in and store the boulder. It will
-	 * call the SetSpeed function to control the front roller's speed, and
-	 * periodically call the CheckSwitch function. If CheckSwitch returns true,
-	 * Stop() is called. TakeBall is controlled by the IntakeBall command.
+	/**
+	 * Controls the front rollers to take in and store the boulder.
+	 * Calls SetSpeed to conrol the roller's speed, and CheckSwitch
+	 * to determine if the rollers should be stopped. If CheckSwitch
+	 * and variable check are both true, Stop is called.
+	 * NOTE: Relies on the IntakeBall command
 	 *
-	 * @param check is used as a safety to determine whether the CheckSwitch
-	 * function should be called, just in case it is broken in the field.
-	 * check is initially set to true.
+	 * @param check is used as a safety measure determine if the
+	 * CheckSwitch function should be called just in case the switch
+	 * itself malfunctions in the field. Check is defaulted to true.
 	 */
 	void TakeBall(bool check = true);
 
-	/** Controls the front rollers in order to horizontally push out the boulder
-	 * from its storage. It will call the SetSpeed function to control the speed
-	 * of the rollers. PushBall is controlled by the OuttakeBall command.
+	/**
+	 * Controls the front rollers in order to horizontally push the
+	 * boulder from its storage. It will call the SetSpeed function to
+	 * control the speed of the rollers.
+	 * NOTE: PushBall is controlled by the OuttakeBall command.
 	 */
 	void PushBall();
 
-	/** Sets the speed and direction of the front roller. The argument speed is a
-	 * double between -1 and 1. 1 and -1 represent the roller's maximum speed in
-	 * two different directions. Negative values for speed indicate that the roller
-	 * should move outward (push), while positive values indicate a move inward (intake)
+	/**
+	 * Sets the speed and direction of the front roller. Values of -1 or
+	 * 1 for speed represent the roller's maximum possible speed in two
+	 * dirtectioms. Negative values indicate that the roller should move
+	 * outward (push ball) while positive values indicate the opposite.
 	 *
 	 * @param speed the speed of the roller, a double between -1.0 and 1.0
 	 */
 	void SetSpeed(double speed);
 
-	/** Returns the current speed of the front roller (a value between -1 and 1)
+	/** Returns the front roller's current speed (a value between -1 and 1)
 	 */
 	double GetSpeed() const;
 
-	/** Calls SetSpeed with argument 0.0 to stop rollers. Called when the End function
-	 * of the IntakeBall command is called, or by the TakeBall function.
+	/**
+	 * Calls SetSpeed with argument 0.0 to stop rollers. Only called by either
+	 * TakeBall or when the IntakeBall command's End function is invoked.
 	 */
 	void Stop();
 
