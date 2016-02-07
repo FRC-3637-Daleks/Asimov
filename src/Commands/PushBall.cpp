f98b05d6751a7dadca335be00ca6b97d3679dc97
David Sevilla <sevilladavid83@gmail.com>
2016-02-07 16:18:48 -0500

    Cleaned up useless comments

--- src/Commands/PushBall.cpp
+++ src/Commands/PushBall.cpp
@@ -14,35 +14,7 @@ PushBall::PushBall(Intake *intake) : CommandGroup("PushBall")
 	this->AddSequential(new WaitCommand(timeout_));
 	this->AddSequential(stop_);
 }
-/*
-bool PushBall::IsFinished()
-{
-	return false;
-}
-/*
-void PushBall::Initialize()
-{
-//	intake_->OutakeBall();
-}
 
-bool PushBall::IsFinished()
-{
-	//wait
-//	return !intake_->CheckSwitch();
-	return push_->IsFinished();
-}
-
-void PushBall::End()
-{
-//	intake_->Stop();
-	// call push end
-}
-
-void PushBall::Interrupted()
-{
-	End();
-}
-*/
 void PushBall::SetTimeout(double timeout)
 {
 	timeout_ = timeout;
@@ -68,26 +40,12 @@ bool PushBall::Push::IsFinished()
 {
 	return !intake_->CheckSwitch();
 }
-/*
-void PushBall::Push::End()
-{
-
-}
-
-void PushBall::Push::Interrupted()
-{
-}*/
 
 // Stopper Command:
 PushBall::Stopper::Stopper(Intake *intake)
 {
 	intake_ = intake;
 }
-/*
-void PushBall::Stopper::Initialize()
-{
-
-}*/
 
 bool PushBall::Stopper::IsFinished()
 {
@@ -98,10 +56,3 @@ void PushBall::Stopper::End()
 {
 	intake_->Stop();
 }
-/*
-void PushBall::Stopper::Interrupted()
-{
-
-}*/
-
-// Stop Command:
