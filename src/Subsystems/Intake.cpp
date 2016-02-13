b168c6421337c851f604f5da8b190fa5e837f62f
David Sevilla <sevilladavid83@gmail.com>
2016-02-13 13:40:46 -0500

    Implements new functions and variable declarations/checks for the subsystem state, mode and maximum velocity

--- src/Subsystems/Intake.cpp
+++ src/Subsystems/Intake.cpp
@@ -1,8 +1,15 @@
 #include "Intake.h"
 #include "Commands/IntakeBall.h"
 #include "Commands/PushBall.h"
+#include <math.h>
 
-using Intake = subsystems::Intake;
+/**
+ * Subsystems namespace with implementation
+ * of Intake class.
+ */
+namespace subsystems
+{
+
 using IntakeBall = commands::IntakeBall;
 using PushBall = commands::PushBall;
 
@@ -11,8 +18,13 @@ Intake::Intake() : Subsystem("Intake")
 {
 	roller_ = new CANTalon(1);
 	detector_ = new DigitalInput(2);
-	intake_speed_ = 1.0; // Default values
+	// Default values:
+	intake_speed_ = 1.0;
 	push_speed_ = -1.0;
+	state_ = State_t::OFF;
+	mode_ = Mode_t::VELOCITY;
+	max_velocity_ = 30;
+	allowed_error_ = 0.1;
 }
 
 // Destructor:
@@ -20,6 +32,21 @@ Intake::~Intake()
 {
 	delete roller_;
 	delete detector_;
+}
+
+// Main functions:
+void Intake::Initialize()
+{
+	roller_->SetFeedbackDevice(CANTalon::QuadEncoder);
+	roller_->ConfigEncoderCodesPerRev(1024);
+	roller_->SetSensorDirection(false);
+	roller_->SelectProfileSlot(0);
+	roller_->SetVoltageRampRate(0.0);
+	roller_->SetCloseLoopRampRate(0.0);
+
+	// Configure max and min voltage outputs
+	roller_->ConfigNominalOutputVoltage(0.0, 0.0);
+	roller_->ConfigPeakOutputVoltage(12.0, -12.0);
 }
 
 bool Intake::CheckSwitch() const
@@ -42,8 +69,14 @@ void Intake::OutakeBall()
 
 void Intake::SetSpeed(double speed)
 {
-	if (speed > -1.0 && speed < 1.0)
+	if (mode_ == Mode_t::VELOCITY)
+	{
+		roller_->Set(max_velocity_ * speed);
+	}
+	else if (mode_ == Mode_t::VBUS)
+	{
 		roller_->Set(speed);
+	}
 }
 
 double Intake::GetSpeed() const
@@ -76,6 +109,60 @@ double Intake::GetPushSpeed() const
 	return push_speed_;
 }
 
+// Error functions:
+double Intake::GetErr() const
+{
+	return roller_->GetSetpoint() - roller_->Get();
+}
+
+void Intake::SetAllowedError(double err)
+{
+	allowed_error_ = err;
+}
+
+double Intake::GetAllowedError() const
+{
+	return allowed_error_;
+}
+
+bool Intake::IsAllowable() const
+{
+	return (fabs(GetErr()) > fabs(allowed_error_));
+}
+
+double Intake::GetMaxVelocity() const
+{
+	return max_velocity_;
+}
+
+void Intake::SetMaxVelocity(double max_velocity)
+{
+	max_velocity_ = max_velocity;
+}
+
+// State functions:
+void Intake::SetState(State_t state)
+{
+	state_ = state;
+}
+
+Intake::State_t Intake::GetState() const
+{
+	return state_;
+}
+
+// Mode functions:
+void Intake::SetMode(Mode_t mode)
+{
+	mode_ = mode;
+}
+
+Intake::Mode_t Intake::GetMode() const
+{
+	return mode_;
+}
+
+// Command functions:
 IntakeBall* Intake::MakeIntakeBall()
 {
 	return new IntakeBall(this);
@@ -85,3 +172,5 @@ PushBall* Intake::MakePushBall()
 {
 	return new PushBall(this);
 }
+
+} // end namespace subsystems
