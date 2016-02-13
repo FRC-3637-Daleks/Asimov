06df3ccdde23ecf3242b03da07f1d8dcbd6ebec3
David Sevilla <sevilladavid83@gmail.com>
2016-02-13 13:44:57 -0500

    Adds the implementation of functions, variable checks, and use of states and modes for the subsystem, and the roller's maximum velocity

--- src/Subsystems/Shooter.cpp
+++ src/Subsystems/Shooter.cpp
@@ -1,11 +1,19 @@
 #include "Shooter.h"
-//include commands/(shooting command)
+#include "Commands/SpinUp.h"
+#include "Commands/SpinDown.h"
+#include <math.h>
 
 using Shooter = subsystems::Shooter;
+using SpinUp = commands::SpinUp;
+using SpinDown = commands::SpinDown;
 
 Shooter::Shooter() : Subsystem("Shooter")
 {
 	top_roller_ = new CANTalon(1);
+	allowed_error_ = 0.1;
+	state_ = State_t::OFF;
+	mode_ = Mode_t::VELOCITY;
+	max_velocity_ = 30;
 }
 
 Shooter::~Shooter()
@@ -31,6 +39,17 @@ void Shooter::SpinUp(double speed)
 {
 	double target = top_roller_->Get() * speed;
 	top_roller_->Set(target);
+
+	///////////////////HALP////////////////////////
+
+	if (mode_ == Mode_t::VELOCITY)
+	{
+		top_roller_->Set(max_velocity_ * speed);
+	}
+	else if (mode_ == Mode_t::VBUS)
+	{
+		top_roller_->Set(speed);
+	}
 }
 
 void Shooter::SpinDown()
@@ -41,4 +60,57 @@ void Shooter::SpinDown()
 void Shooter::EmergencyStop()
 {
 	top_roller_->StopMotor();
+}
+
+double Shooter::GetErr() const
+{
+	return top_roller_->GetSetpoint() - top_roller_->Get();
+}
+
+void Shooter::SetAllowedError(double err)
+{
+	allowed_error_ = err;
+}
+
+double Shooter::GetAllowedError() const
+{
+	return allowed_error_;
+}
+
+bool Shooter::IsAllowable() const
+{
+	return (fabs(GetErr()) > fabs(allowed_error_));
+}
+
+
+// States and modes or whatever:
+Shooter::State_t Shooter::GetState() const
+{
+	return state_;
+}
+
+void Shooter::SetState(State_t state)
+{
+	state_ = state;
+}
+
+double Shooter::GetMaxVelocity() const
+{
+	return max_velocity_;
+}
+
+void Shooter::SetMaxVelocity(double max_velocity)
+{
+	max_velocity_ = max_velocity;
+}
+
+// Command functions:
+SpinUp* Shooter::MakeSpinUp()
+{
+	return new commands::SpinUp(this, 1.0);
+}
+
+SpinDown* Shooter::MakeSpinDown()
+{
+	return new commands::SpinDown(this);
 }
