e7f77bbac41b8a2f0d299300ebbe22bf54d6c1de
David Sevilla <sevilladavid83@gmail.com>
2016-02-13 16:49:34 -0500

    Changes existing functions to take Intake states into account, and updates documentation.

--- src/Commands/IntakeBall.cpp
+++ src/Commands/IntakeBall.cpp
@@ -1,16 +1,31 @@
 #include "IntakeBall.h"
 
-using IntakeBall = commands::IntakeBall;
+/**
+ * Namespace commands with implementation
+ * of IntakeBall command functions.
+ */
+namespace commands
+{
 
 // Constructor:
 IntakeBall::IntakeBall(Intake * intake) : Command("IntakeBall")
 {
 	intake_ = intake;
+	SetInterruptible(true);
 }
 
+// Main functions:
 void IntakeBall::Initialize()
 {
-	intake_->TakeBall(true);
+	if (intake_->GetState() == State_t::OFF)
+	{
+		intake_->SetState(State_t::TAKING);
+		intake_->TakeBall(true);
+	}
+	else
+	{
+		std::cout << "ERROR: Invalid starting state (Should be \"OFF\")";
+	}
 }
 
 bool IntakeBall::IsFinished()
@@ -21,9 +36,13 @@ bool IntakeBall::IsFinished()
 void IntakeBall::End()
 {
 	intake_->Stop();
+	intake_->SetState(State_t::HOLDING);
 }
 
 void IntakeBall::Interrupted()
 {
-	End();
+	intake_->Stop();
+	intake_->SetState(State_t::OFF);
 }
+
+} // end namespace commands
