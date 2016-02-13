662bdb4ff6a29c601e3276f9e38ed4ee217329e3
Eric Ward <ezeward4@gmail.com>
2016-02-13 15:53:25 -0500

    added modes
    Added velocity and vbus  modes

--- src/Subsystems/Swiss.h
+++ src/Subsystems/Swiss.h
@@ -5,8 +5,8 @@
  *      Author: elija_000
  */
 
-#include <CANTalon.h>
-#include <Talon.h>
+
+
 #include "WPILib.h"
 
 
@@ -15,7 +15,6 @@
 
 #include <memory>
 
-#define PI 3.1415926535897
 
 namespace subsystems
 {
@@ -23,18 +22,32 @@ namespace subsystems
 class Swiss: public Subsystem
 {
 public:
-	enum state_t {max=0, door, cheval, min, end};
-
+	enum state_t {max=0, door, cheval, min, doordown, end};
+	enum mode_t {pos = 0, velocity, vbus};
+	struct PIDValues{
+		double p;
+		double i;
+		double d;
+	} pPid, vPid;
 private:
+	static double tickToDegree;
 	static double states[4];
+	static double maxVelocity;
 	state_t position;
+	mode_t mode;
 	CANTalon *swisstalon;
 
 public:
+
 	Swiss(int deviceNumber, double P, double D);
+	void SetPIDValues(double p1, double p2, double v1, double v2, double b1, double b2);
+	void SetVelocity(double v);
+	void SetVoltage(double v);
+	void SetMode(mode_t mode);
 	void SetState(state_t s);
 	void MaxHeight();
 	void MinHeight();
+	void LowerPort();
 	void LiftDoor();
 	void LowerCheval();
 	state_t GetState();
