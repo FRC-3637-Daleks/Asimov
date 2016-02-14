33a92145c6314de456d0324e61eec2ef09c97d72
David Sevilla <sevilladavid83@gmail.com>
2016-02-13 19:04:43 -0500

    Authors the implementation of the SpinUp command.

---
+++ src/Commands/SpinUp.cpp
@@ -0,0 +1,59 @@
+#include "SpinUp.h"
+
+/**
+ * Commands namespace with implementation
+ * of SpinUp command functions.
+ */
+namespace commands
+{
+
+// Constructor:
+SpinUp::SpinUp(Shooter *shooter, double speed) : Command("Spin Up")
+{
+	shooter_ = shooter;
+	speed_ = speed;
+}
+
+// Main functions:
+void SpinUp::Initialize()
+{
+	State_t currState = shooter_->GetState(); // For readability
+	if (currState == State_t::OFF || currState == State_t::SPINNINGUP)
+	{
+		shooter_->SpinUp(speed_);
+		if (currState != State_t::SPINNINGUP)
+			shooter_->SetState(State_t::SPINNINGUP);
+	}
+	else
+	{
+		std::cout << "ERROR: Invalid starting state (should be \"OFF\" or \"SPINNINGUP\")";
+	}
+}
+
+bool SpinUp::IsFinished()
+{
+	return !shooter_->IsAllowable();
+}
+
+void SpinUp::End()
+{
+	shooter_->SetState(State_t::SPUNUP);
+}
+
+void SpinUp::Interrupted()
+{
+	shooter_->EmergencyStop();
+	shooter_->SetState(State_t::OFF);
+}
+
+void SpinUp::SetSpeed(double speed)
+{
+	speed_ = speed;
+}
+
+double SpinUp::GetSpeed() const
+{
+	return speed_;
+}
+
+}// end namespace commands
