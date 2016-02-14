38874021a53fde59b6151030a1784b665a88cf65
David Sevilla <sevilladavid83@gmail.com>
2016-02-14 15:02:43 -0500

    Completes the Shoot implementation, declaration and documentation to shoot the boulder from it's storage using both the Intake and Shooter subsytems.

--- src/Commands/Shoot.h
+++ src/Commands/Shoot.h
@@ -8,6 +8,8 @@
 #ifndef SRC_COMMANDS_SHOOT_H_
 #define SRC_COMMANDS_SHOOT_H_
 #include "Subsystems/Shooter.h"
+#include "Subsystems/Intake.h"
+#include "WPILib.h"
 
 /**
  * Commands namespace with declaration
@@ -17,48 +19,68 @@ namespace commands
 {
 
 using Shooter = subsystems::Shooter;
+using Intake = subsystems::Intake;
 using State_t = Shooter::State_t;
 
 class Shoot : public Command
 {
 // Constructor & destructor:
 public:
-	Shoot(Intake* intake, Shooter* shooter);
+	Shoot(Intake* intake, Shooter* shooter, double shoot_time);
 	virtual ~Shoot() = default;
 
 // Main functions:
 public:
 	/**
-	 * Sets up shoot command before executiion each time it is called.
-	 *
+	 * Sets up shoot command. Prints an error message if current state
+	 * is not SPUNUP. Otherwise, changes the states of both Intake
+	 * and Shooter to SHOOTING and calls Intake's ShootBall function
+	 * to spin the front rollers inwards for a certain time.
 	 */
 	void Initialize() override;
 
 	/**
-	 *
+	 * Not implemented.
 	 */
-	void Execute() override;
+	void Execute() override {};
 
 	/**
-	 *
+	 * Returns true if the current timer value is greater than the time
+	 * allowed for the rollers to spin, false if otherwise.
 	 */
 	bool IsFinished() override;
 
 	/**
-	 *
+	 * Sets the state of Shooter to SPUNUP and the state of Intake to OFF.
+	 * Turns off the front rollers.
 	 */
 	void End() override;
 
 	/**
-	 *
+	 * Calls the end function if another command needs to use the Shooter or
+	 * Intake subsystems.
 	 */
 	void Interrupted() override;
+
+	/**
+	 * Sets the time allowed to spend moving the rollers for shooting the ball.
+	 * @param shoot_time the new time until End is called.
+	 */
+	void SetShootTime(double shoot_time);
+
+	/**
+	 * @return shoot_time_ the time until End is called. How long the rollers will
+	 * spin.
+	 */
+	double GetShootTime() const;
 
 protected:
 
 private:
 	Intake *intake_;
 	Shooter *shooter_;
+	double shoot_time_;
+	Timer *timer_;
 };
 
 } // end namespace commands
